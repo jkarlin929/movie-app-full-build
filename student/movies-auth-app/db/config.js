c4b3a81a103583dbc575060f352e8ae5166bccf6
Joe Keohah <jkeohan@gmail.com>
2018-01-02 09:30:48 -0500

    updated db name

--- student/movies-auth-app/db/config.js
+++ student/movies-auth-app/db/config.js
@@ -13,7 +13,7 @@ const pgp = require('pg-promise')(options)
 function setDatabase() {
   if(process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     return pgp({
-      database:'movies_auth_dev',
+      database:'movies_auth_development',
       port:5432,
       host:'localhost'
     });
